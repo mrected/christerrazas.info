9b6072e33d5194490df12c4cb3db5ac1022baa2f
Chris Terrazas <chris.terrazas@gmail.com>
2017-12-13 08:31:11 -0500

    locked in sizes for top divs and started work on nav links

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
 $('#menuToggle').click(function(){
   $(this).toggleClass('open');
-  $('#nav').toggleClass('open');
+  // $('#nav').toggleClass('open');
+})
+
+// $('#menuToggle').toggle(function(){
+//   $('#navbarLinks').addClass('open');
+//   // $('#nav').toggleClass('open');
+// });
+
+$('#menuToggle').click(function(){
+	$('#navbarLinks').toggle("fast");
 })
