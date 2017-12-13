3e6bf1590abd71036c9f3f315f11ad34756d6fbb
Chris Terrazas <chris.terrazas@gmail.com>
2017-12-12 22:20:22 -0500

    started work on image viewer

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,16 @@ $('#menuToggle').click(function(){
   $(this).toggleClass('open');
   $('#nav').toggleClass('open');
 })
+
+const imgHeight = $('#selectedTopImg').height();
+const imgWidth = $('#selectedTopImg').width();
+$('#selectedTopImg').height(Math.round(imgHeight)).width(Math.round(imgWidth));
+
+
+console.log(`height ${$('#selectedTopImg').height()} width ${$('#selectedTopImg').width()}`);
+
+$('#topImg').mousemove(function(event){
+	let imgCoordsX = event.pageX;
+	let imgCoordsY = event.pageY;
+	// console.log(`X: ${imgCoordsX} Y: ${imgCoordsY}`);
+})
