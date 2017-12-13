486051c93c643e8309ac38e473ec3c763c1b4698
Chris Terrazas <chris.terrazas@gmail.com>
2017-12-13 16:41:09 -0500

    worked on top img viewer. scroll, size and positioning are all off. needs work

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,26 @@
-const imgHeight = $('#selectedTopImg').height();
-const imgWidth = $('#selectedTopImg').width();
-$('#selectedTopImg').height(Math.round(imgHeight)).width(Math.round(imgWidth));
+// const imgHeight = $('#selectedTopImg').height();
+// const imgWidth = $('#selectedTopImg').width();
+// $('#selectedTopImg').height(Math.round(imgHeight)).width(Math.round(imgWidth));
 
 
 // console.log(`height ${$('#selectedTopImg').height()} width ${$('#selectedTopImg').width()}`);
 
 $('#topImg').mousemove(function(event){
-	let imgCoordsX = event.pageX;
-	let imgCoordsY = event.pageY;
-	console.log(`X: ${imgCoordsX} Y: ${imgCoordsY}`);
-})
+	clearTimeout($.data(this, 'moveTimer'));
+	$.data(this, 'moveTimer', setTimeout(function(){
+		let imgCoordsX = (event.pageX/4);
+		let imgCoordsY = (event.pageY/4);
+		// $("#selectedTopImg").animate({left: (imgCoordsX-1300) + "px"}, 1000);
+	},500));
+
+	// let imgCoordsX = (event.pageX/4);
+	// let imgCoordsY = (event.pageY/4);
+	// // $("#selectedTopImg").animate({left: imgCoordsX + "px"}, 500);
+
+});
+
+// $("#topImg").click(function(){
+	
+	
+// })
+
