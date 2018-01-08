1af31feab3443716fba24e36e40a44f86c3328b7
Chris Terrazas <chris.terrazas@gmail.com>
2018-01-08 10:59:32 -0500

    made top nav image scroll down and sideways on scroll in truly awesome fashion

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,28 @@ $(".navLink").click(function(){
       });
 
 });
+
+let selectedTopImgX = -45;
+let selectedTopImgY = -12;
+$("#selectedTopImg").css(`background-position`,`${selectedTopImgX}rem ${selectedTopImgY}rem`);
+
+$(window).scroll(function(){
+	let x = ($(window).scrollTop()/20)+selectedTopImgX;
+	let y = ($(window).scrollTop()/50)+selectedTopImgY;
+	// console.log(`${x}rem ${y}rem`);
+	$("#selectedTopImg").css(`background-position`,`${x}rem ${y}rem`);
+	console.log($(window).scrollTop());
+});
+
+
+
+//fixes jumpy scroll over fixed backround in ie11
+if(navigator.userAgent.match(/Trident\/7\./)) { // if IE
+        $('body').on("mousewheel", function () {
+            event.preventDefault(); 
+
+            let wheelDelta = event.wheelDelta;
+            let currentScrollPosition = window.pageYOffset;
+            window.scrollTo(0, currentScrollPosition - wheelDelta);
+        });
+}
